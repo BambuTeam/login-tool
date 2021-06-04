18c3776adc52160f9319ef34ec63d3fea5e6dfaa
Luis Enrique Concuá <hola@bambu.team>
2021-06-04 12:58:35 -0600

    Start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import HeroCarousel from './pages/HeroCarousel';
+
+function App() {
+  return (
+    <div className="App">
+      < HeroCarousel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HeroCarousel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HeroCarousel from "react-hero-carousel";
+
+export default () => (
+  <HeroCarousel interval={8000} >
+    <img className="sizecarousel-color1"
+      width="100%"
+      height="100%"
+    />
+    <img className="sizecarousel-color1"
+      
+      width="100%"
+      height="100%"
+    />
+    <img className="sizecarousel-color1"
+      
+      width="100%"
+      height="100%"
+    />
+  </HeroCarousel>
+);
